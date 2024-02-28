90c79ca566cde38445d214ea8dd49ebb27765f62
Goran P <nallenrockar123@gmail.com>
2024-02-28 11:36:51 +0100

    byggde upp min struktur

--- DropDown-2/README.md
+++ DropDown-2/README.md
@@ -1,2 +1,9 @@
 # DropDown2
 
+1. göra alla filer man behöver
+* Json filen med data -
+* skall alla vara i samma mapp? -
+* försök att göra kopling med scss -
+
+
+2.  
---
+++ DropDown-2/src/App.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import DropDown from "./Components/DropDown/DropDown";
+import InfoBox from "./Components/InfoBox/InfoBox";
+import Worker from "./Components/JSON/bob_simple_10.json";
